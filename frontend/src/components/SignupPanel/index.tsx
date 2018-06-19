5dca91ade1d995be2b15e55a244925eb80ef42d6
chmaltsp <chmaltsp@gmail.com>
2018-06-19 01:45:59 -0400

    add email button and validation styles

--- frontend/src/components/SignupPanel/index.tsx
+++ frontend/src/components/SignupPanel/index.tsx
@@ -5,12 +5,15 @@ import { em } from 'polished';
 import Form from './Form';
 
 const Wrapper = styled.div`
-  padding: ${em(190)} ${em(320)};
+  margin: 0 auto;
+  padding: ${em(190)} 0;
+  max-width: ${em(780)};
 `;
 
 const CtaText = styled.h2`
   font-weight: 800;
   font-size: ${em(32)};
+  line-height: 1.4;
 `;
 // export interface SignupPanelProps {}
 
