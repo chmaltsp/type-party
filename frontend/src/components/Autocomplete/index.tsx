274e71c046def32e3d2d963b387b68a759fa58ce
chmaltsp <chmaltsp@gmail.com>
2018-11-15 00:41:44 -0500

    add tag and onEnter input callback

--- frontend/src/components/Autocomplete/index.tsx
+++ frontend/src/components/Autocomplete/index.tsx
@@ -1,13 +1,24 @@
 import * as React from 'react';
 import styled from 'sc';
 
-import Downshift, { ControllerStateAndHelpers, GetItemPropsOptions } from 'downshift';
+import { ControllerStateAndHelpers, GetItemPropsOptions } from 'downshift';
 
-import { InputBase, InputWrapper, Label } from '../Input';
+import { InputBase, InputWrapper as AutoCompleteWrapper, Label } from '../Input';
 
+import Flex from '../Flex';
+import Tag from '../Tag';
+import MultiDownshift from './MultiDownshift';
 import { ListItem, ListItemProps, ListWrapper } from './styles';
 
-const Input = styled(InputBase)``;
+const Input = styled.input`
+  flex: 1;
+`;
+
+const InputWrapper = styled(Flex)`
+  border: 1px solid ${({ theme }) => theme.colors.greyCCC};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  flex-wrap: wrap;
+`;
 
 export interface AutocompleteProps {}
 
@@ -23,6 +34,11 @@ const items = [
 
 type ItemWithExtraProps = GetItemPropsOptions<ListItemProps>;
 
+interface MultiDownshiftProps {
+  removeItem: any;
+  getRemoveButtonProps: any;
+  selectedItems: any;
+}
 export default class Autocomplete extends React.Component<
   AutocompleteProps,
   AutocompleteState
@@ -37,27 +53,53 @@ export default class Autocomplete extends React.Component<
     console.log('You selected', selection);
   }
 
+  private handleOnInputEnter = (event: React.KeyboardEvent) => {
+    // Callback on Enter only to add to list etc...
+    console.log('KEYPRESS', event.which);
+    if (event.which === 13) {
+      console.log('ON ENTER', event.which);
+      event.preventDefault();
+    }
+  }
+
   public render() {
     return (
-      <Downshift onChange={this.handleOnchange} itemToString={item => item.value}>
+      <MultiDownshift
+        onChange={this.handleOnchange}
+        itemToString={(item: any) => (item ? item.value : '')}
+      >
         {({
           getRootProps,
           getInputProps,
           getItemProps,
           getLabelProps,
           getMenuProps,
+          getRemoveButtonProps,
+          removeItem,
           isOpen,
           inputValue,
           highlightedIndex,
-          selectedItem,
-        }: ControllerStateAndHelpers<ListItemProps>) => (
-          <InputWrapper
+          selectedItems,
+        }: ControllerStateAndHelpers<ListItemProps> & MultiDownshiftProps) => (
+          <AutoCompleteWrapper
             {...getRootProps({
               refKey: 'innerRef',
             })}
           >
             <Label {...getLabelProps()}>Enter a fruit</Label>
-            <Input {...getInputProps()} />
+            <InputWrapper>
+              {selectedItems.map((selectedItem: ListItemProps, index: number) => (
+                <Tag
+                  name={selectedItem.value}
+                  removeButtonProps={getRemoveButtonProps({ item: selectedItem })}
+                />
+              ))}
+              <Input
+                {...getInputProps({
+                  onKeyDown: this.handleOnInputEnter,
+                })}
+              />
+            </InputWrapper>
             <ListWrapper {...getMenuProps()}>
               {isOpen
                 ? items
@@ -81,9 +123,9 @@ export default class Autocomplete extends React.Component<
                     ))
                 : null}
             </ListWrapper>
-          </InputWrapper>
+          </AutoCompleteWrapper>
         )}
-      </Downshift>
+      </MultiDownshift>
     );
   }
 }
