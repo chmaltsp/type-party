8577cc7c0266ca4117b1f5b3eb820e92a232dedc
chmaltsp <chmaltsp@gmail.com>
2018-11-15 00:41:44 -0500

    add tag and onEnter input callback

---
+++ frontend/src/components/Autocomplete/MultiDownshift.tsx
@@ -0,0 +1,133 @@
+import Downshift, {
+  ControllerStateAndHelpers,
+  DownshiftState,
+  StateChangeOptions,
+} from 'downshift';
+import * as React from 'react';
+import { ListItemProps } from './styles';
+
+interface MultiDownshiftState {
+  selectedItems: string[];
+}
+
+interface MultiDownshiftProps {
+  render?: () => void;
+  children?: any;
+  onChange: (
+    selectedItems: string[],
+    downshift: ControllerStateAndHelpers<ListItemProps>
+  ) => void;
+  onSelect?: (
+    selectedItems: string[],
+    downshift: ControllerStateAndHelpers<ListItemProps>
+  ) => void;
+  itemToString?: (item: any) => string;
+}
+
+interface RemoveButtonProps {
+  onClick: (e: React.SyntheticEvent) => void;
+  item: string;
+}
+
+class MultiDownshift extends React.Component<MultiDownshiftProps, MultiDownshiftState> {
+  public state = { selectedItems: [] };
+
+  public stateReducer = (
+    state: DownshiftState<ListItemProps>,
+    changes: StateChangeOptions<ListItemProps>
+  ) => {
+    switch (changes.type) {
+      case Downshift.stateChangeTypes.keyDownEnter:
+      case Downshift.stateChangeTypes.clickItem:
+        return {
+          ...changes,
+          highlightedIndex: state.highlightedIndex,
+          inputValue: '',
+          isOpen: true,
+        };
+      default:
+        return changes;
+    }
+  }
+
+  public handleSelection = (
+    selectedItem: string,
+    downshift: ControllerStateAndHelpers<ListItemProps>
+  ) => {
+    const callOnChange = () => {
+      const { onSelect, onChange } = this.props;
+      const { selectedItems } = this.state;
+      if (onSelect) {
+        onSelect(selectedItems, this.getStateAndHelpers(downshift));
+      }
+      if (onChange) {
+        onChange(selectedItems, this.getStateAndHelpers(downshift));
+      }
+    };
+    // @ts-ignore
+    if (this.state.selectedItems.includes(selectedItem)) {
+      this.removeItem(selectedItem, callOnChange);
+    } else {
+      this.addSelectedItem(selectedItem, callOnChange);
+    }
+  }
+
+  public removeItem = (item: any, cb?: any) => {
+    this.setState(({ selectedItems }) => {
+      return {
+        selectedItems: selectedItems.filter(i => i !== item),
+      };
+    },            cb);
+  }
+  public addSelectedItem(item: any, cb: any) {
+    this.setState(
+      ({ selectedItems }) => ({
+        selectedItems: [...selectedItems, item],
+      }),
+      cb
+    );
+  }
+
+  public getRemoveButtonProps = ({ onClick, item, ...props }: RemoveButtonProps) => {
+    return {
+      onClick: (e: any) => {
+        console.log(e, item);
+        // TODO: use something like downshift's composeEventHandlers utility instead
+        // tslint:disable-next-line:no-unused-expression
+        onClick && onClick(e);
+        e.stopPropagation();
+        this.removeItem(item);
+      },
+      ...props,
+    };
+  }
+
+  public getStateAndHelpers(downshift: ControllerStateAndHelpers<ListItemProps>): any {
+    const { selectedItems } = this.state;
+    return {
+      getRemoveButtonProps: this.getRemoveButtonProps,
+      removeItem: this.removeItem,
+      selectedItems,
+      ...downshift,
+    };
+  }
+  public render() {
+    // const { render, children = render, ...props } = this.props;
+    // TODO: compose together props (rather than overwriting them) like downshift does
+    return (
+      <Downshift
+        {...this.props}
+        stateReducer={this.stateReducer}
+        onChange={this.handleSelection}
+        selectedItem={null}
+      >
+        {(downshift: ControllerStateAndHelpers<ListItemProps>) =>
+          // @ts-ignore
+          this.props.children(this.getStateAndHelpers(downshift))
+        }
+      </Downshift>
+    );
+  }
+}
+
+export default MultiDownshift;
---
+++ frontend/src/components/Tag/index.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+
+import styled from 'sc';
+
+import FlexBase from '../Flex';
+
+import closeImg from './close-x.svg';
+
+const Wrapper = styled(FlexBase)`
+  background-color: ${({ theme }) => theme.colors.black};
+  padding: ${({ theme }) => theme.spacing.xs}px;
+`;
+
+const Text = styled.span`
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+const RemoveButton = styled.img`
+  cursor: pointer;
+`;
+
+interface TagProps {
+  removeButtonProps: any;
+  name: string;
+}
+
+const Tag: React.SFC<TagProps> = ({ name, removeButtonProps }) => {
+  return (
+    <Wrapper>
+      <Text>{name}</Text>
+      <RemoveButton src={closeImg} {...removeButtonProps} />
+    </Wrapper>
+  );
+};
+
+export default Tag;
