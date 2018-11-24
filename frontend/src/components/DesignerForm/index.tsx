217c7e8a34c9ca7f35797570838d17ee7376d296
chmaltsp <chmaltsp@gmail.com>
2018-11-24 00:28:27 -0500

    move foundries one level up

--- frontend/src/components/DesignerForm/index.tsx
+++ frontend/src/components/DesignerForm/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import { Field, FieldProps, Formik, FormikProps } from 'formik';
 
+import Autocomplete from '../Autocomplete';
 import Button from '../Button';
 import Foundry from '../FoundryForm';
 import Input from '../Input';
@@ -14,21 +15,11 @@ export interface DesignerFormState {
   showFoundryForm: boolean;
 }
 interface InputValues {
-  designerName: string;
-  designerUrl: string;
-  foundry: string;
+  name: string;
+  url: string;
+  foundries: string[];
 }
 export default class DesignerForm extends React.PureComponent<DesignerFormProps, any> {
-  public state = {
-    showFoundryForm: false,
-  };
-
-  private toggleFoundryForm = () => {
-    this.setState({
-      showFoundryForm: !this.state.showFoundryForm,
-    });
-  }
-
   private handleOnSubmit = (values: any) => {
     console.log(values);
   }
@@ -36,32 +27,26 @@ export default class DesignerForm extends React.PureComponent<DesignerFormProps,
     return (
       <Formik
         initialValues={{
-          designerName: '',
-          designerUrl: '',
-          foundry: '',
+          foundries: [],
+          name: '',
+          url: '',
         }}
         onSubmit={this.handleOnSubmit}
         render={(props: FormikProps<InputValues>) => {
           return (
             <div>
               <Field
-                name="designerName"
+                name="name"
                 render={(fieldProps: FieldProps<InputValues>) => {
                   return <Input label="Designer Name" {...fieldProps} />;
                 }}
               />
               <Field
-                name="designerUrl"
+                name="url"
                 render={(fieldProps: FieldProps<InputValues>) => {
                   return <Input label="Designer Url" {...fieldProps} />;
                 }}
               />
-              <div>
-                <Button rounded={true} type="button" onClick={this.toggleFoundryForm}>
-                  + Add Foundry
-                </Button>
-              </div>
-              {this.state.showFoundryForm && <Foundry />}
               <Button type="button" onClick={props.submitForm}>
                 Submit Designer
               </Button>
--- frontend/src/pages/AddTypeface/Form.tsx
+++ frontend/src/pages/AddTypeface/Form.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import styled from 'sc';
 
 import { Field, FieldProps, Form as FormBase, Formik, FormikProps } from 'formik';
+import Autocomplete from '../../components/Autocomplete';
 import ButtonBase from '../../components/Button';
 import DesignerForm from '../../components/DesignerForm';
 import Flex from '../../components/Flex';
+import FoundryForm from '../../components/FoundryForm';
 import Input from '../../components/Input';
 
 export interface TypefaceFormProps {
@@ -13,6 +15,7 @@ export interface TypefaceFormProps {
 
 interface TypefaceFormState {
   showDesignerForm: boolean;
+  showFoundryForm: boolean;
 }
 
 export interface InputValues {
@@ -36,11 +39,18 @@ export default class TypefaceForm extends React.PureComponent<
 > {
   public state = {
     showDesignerForm: false,
+    showFoundryForm: false,
   };
 
   private toggleDesignerForm = () => {
     this.setState({
       showDesignerForm: !this.state.showDesignerForm,
+    });
+  }
+
+  private toggleFoundryForm = () => {
+    this.setState({
+      showFoundryForm: !this.state.showFoundryForm,
     });
   }
   public handleOnSubmit = (values: any) => {
@@ -77,12 +87,48 @@ export default class TypefaceForm extends React.PureComponent<
                   return <Input textarea={true} label="Description" {...fieldProps} />;
                 }}
               />
+              <Field
+                name="designers"
+                render={(fieldProps: FieldProps<InputValues>) => {
+                  return (
+                    <Autocomplete<{ name?: string; value: string }>
+                      items={[{ value: 'jim' }]}
+                      label="Designer(s)"
+                      handleOnChange={selection =>
+                        fieldProps.form.setFieldValue('desingers', selection)
+                      }
+                      itemToString={item => (item && item.name) || ''}
+                    />
+                  );
+                }}
+              />
               <div>
                 <ButtonBase rounded={true} onClick={this.toggleDesignerForm}>
                   + Add Designer
                 </ButtonBase>
               </div>
               {this.state.showDesignerForm && <DesignerForm />}
+              <Field
+                name="foundries"
+                render={(fieldProps: FieldProps<InputValues>) => {
+                  return (
+                    <Autocomplete<{ name?: string; value: string }>
+                      items={[{ value: 'Google' }]}
+                      label="Foundries(s)"
+                      handleOnChange={selection =>
+                        fieldProps.form.setFieldValue('foundries', selection)
+                      }
+                      itemToString={item => (item && item.name) || ''}
+                    />
+                  );
+                }}
+              />
+              <div>
+                <ButtonBase rounded={true} type="button" onClick={this.toggleFoundryForm}>
+                  + Add Foundry
+                </ButtonBase>
+              </div>
+              {this.state.showFoundryForm && <FoundryForm />}
               <ButtonWrapper>
                 <Publish type="button" black={true} onClick={props.submitForm}>
                   Submit Typeface
