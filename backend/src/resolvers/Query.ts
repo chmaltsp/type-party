093955c49eaa196ce3e34f995c6acf5a5f4bb9b2
chmaltsp <chmaltsp@gmail.com>
2018-11-25 19:48:53 -0500

    update schema

--- backend/src/resolvers/Query.ts
+++ backend/src/resolvers/Query.ts
@@ -1,27 +1,6 @@
 import { ctxUser, Context } from '../utils';
 
 export const Query = {
-  feed(parent, args, ctx: Context, info) {
-    return ctx.db.query.posts({ where: { isPublished: true } }, info);
-  },
-
-  drafts(parent, args, ctx: Context, info) {
-    const { userId: id } = ctxUser(ctx);
-
-    const where = {
-      isPublished: false,
-      author: {
-        id,
-      },
-    };
-
-    return ctx.db.query.posts({ where }, info);
-  },
-
-  post(parent, { id }, ctx: Context, info) {
-    return ctx.db.query.post({ where: { id: id } }, info);
-  },
-
   me(parent, args, ctx: Context, info) {
     const { userId: id } = ctxUser(ctx);
     return ctx.db.query.user({ where: { id } }, info);
