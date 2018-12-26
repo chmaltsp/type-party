902f6fc65c5ff239d7643ee0bb7d76025e4e62c0
chmaltsp <chmaltsp@gmail.com>
2018-12-25 20:10:46 -0600

    add getToken helper

--- frontend/src/utils/auth.ts
+++ frontend/src/utils/auth.ts
@@ -3,3 +3,5 @@ const TOKEN_KEY = 'tpt';
 export const removeToken = () => localStorage.removeItem(TOKEN_KEY);
 
 export const setToken = (jwt: string) => localStorage.setItem(TOKEN_KEY, jwt);
+
+export const getToken = () => localStorage.getItem(TOKEN_KEY);
