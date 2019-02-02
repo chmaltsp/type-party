735eb3d6b045e540732d9ae1dcdca86007618409
chmaltsp <chmaltsp@gmail.com>
2019-02-02 18:56:01 -0500

    Add keep exploring

--- frontend/src/components/CardCarousel/index.tsx
+++ frontend/src/components/CardCarousel/index.tsx
@@ -1,20 +1,33 @@
 import * as React from 'react';
-import Card, { CardProps } from '../Card';
-import Flex from '../Flex';
+import styled from 'sc';
+import CardBase, { CardProps } from '../Card';
+import FlexBase from '../Flex';
 
 export interface CardCarouselProps {
   cards: CardProps[];
 }
 
+const Wrapper = styled(FlexBase)`
+  > div {
+    width: 31%;
+    padding-right: ${({ theme }) => theme.spacing.md}px;
+  }
+`;
+
+const Card = styled(CardBase)`
+  img {
+    height: 271px;
+  }
+`;
 export class CardCarousel extends React.PureComponent<CardCarouselProps, any> {
   public render() {
     const { cards } = this.props;
     return (
-      <Flex>
+      <Wrapper>
         {cards.map(card => (
           <Card key={card.title} {...card} />
         ))}
-      </Flex>
+      </Wrapper>
     );
   }
 }
--- frontend/src/pages/Website/__generated__/KeepExploringQuery.ts
+++ frontend/src/pages/Website/__generated__/KeepExploringQuery.ts
@@ -5,11 +5,30 @@
 // GraphQL query operation: KeepExploringQuery
 // ====================================================
 
+export interface KeepExploringQuery_websites_images_thumbnail {
+  __typename: "File";
+  createdAt: any;
+  url: string;
+}
+
+export interface KeepExploringQuery_websites_images {
+  __typename: "Images";
+  thumbnail: KeepExploringQuery_websites_images_thumbnail | null;
+}
+
+export interface KeepExploringQuery_websites_typefaces {
+  __typename: "Typeface";
+  slug: string;
+  name: string;
+}
+
 export interface KeepExploringQuery_websites {
   __typename: "Website";
   title: string;
   slug: string | null;
   url: string;
+  images: KeepExploringQuery_websites_images | null;
+  typefaces: KeepExploringQuery_websites_typefaces[] | null;
 }
 
 export interface KeepExploringQuery {
