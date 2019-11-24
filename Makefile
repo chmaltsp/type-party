282ace0423347d3feed157551664cfb0bc74fc00
chmaltsp <chmaltsp@gmail.com>
2019-11-24 00:40:52 -0500

    add env vars

--- Makefile
+++ Makefile
@@ -1,13 +1,21 @@
-.PHONY: tag-gql-image build-tp-gql push-tp-gql tag-and-push
+.PHONY: tag-gql-image build-tp-gql push-tp-gql tag-and-push set-ssm-param
+
 tag-gql-image:
 	@echo "LOGIN TO DOCKER"
-	eval $(aws ecr --profile tp-access get-login --region us-east-1 --no-include-email)
+	$$(aws ecr --profile tp-access get-login --region us-east-1 --no-include-email)
+	$(eval REV=$(shell git rev-parse HEAD | cut -c1-7))
+	docker tag tp-gql:latest 561034361591.dkr.ecr.us-east-1.amazonaws.com/tp-gql:latest
+	docker tag tp-gql:latest 561034361591.dkr.ecr.us-east-1.amazonaws.com/tp-gql:$(REV)
 
 build-tp-gql:
 	docker build -t tp-gql ./backend
 
 push-tp-gql:
-	docker push 561034361591.dkr.ecr.us-east-1.amazonaws.com/tp-gql
+	$(eval REV=$(shell git rev-parse HEAD | cut -c1-7))
+	docker push 561034361591.dkr.ecr.us-east-1.amazonaws.com/tp-gql:latest
+	docker push 561034361591.dkr.ecr.us-east-1.amazonaws.com/tp-gql:${REV}
 
 tag-and-push-gql: build-tp-gql tag-gql-image push-tp-gql
 
+set-ssm-param: 
+	AWS_PROFILE=tp-access aws ssm put-parameter --name="${NAME}" --value="${VALUE}" --overwrite --type="${TYPE}" --region="us-east-1" 
--- infrastructure/lib/tp-gql.ts
+++ infrastructure/lib/tp-gql.ts
@@ -4,6 +4,8 @@ import cdk = require('@aws-cdk/core');
 import ecs_patterns = require('@aws-cdk/aws-ecs-patterns');
 import { IRepository } from '@aws-cdk/aws-ecr';
 
+import ssm = require('@aws-cdk/aws-ssm');
+
 interface TpGqlProps extends cdk.StackProps {
   vpc: ec2.Vpc;
   ecrRepository: IRepository;
@@ -20,17 +22,56 @@ export class TpGql extends cdk.Stack {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
     });
 
+    const environmentVarKeys = {
+      staging: {
+        PRISMA_SECRET: '/TpStaging/PRISMA_SECRET',
+        PRISMA_MANAGEMENT_API_SECRET: '/TpStaging/PRISMA_MANAGEMENT_API_SECRET',
+        APP_SECRET: '/TpStaging/APP_SECRET',
+        PRISMA_ENDPOINT: '/TpStaging/PRISMA_ENDPOINT',
+      },
+    };
+
+    const PRISMA_SECRET = ssm.StringParameter.fromStringParameterName(
+      this,
+      'PRISMA_SECRET',
+      environmentVarKeys.staging.PRISMA_SECRET
+    ).stringValue;
+
+    const APP_SECRET = ssm.StringParameter.fromStringParameterName(
+      this,
+      'APP_SECRET',
+      environmentVarKeys.staging.APP_SECRET
+    ).stringValue;
+
+    const PRISMA_MANAGEMENT_API_SECRET = ssm.StringParameter.fromStringParameterName(
+      this,
+      'PRISMA_MANAGEMENT_API_SECRET',
+      environmentVarKeys.staging.PRISMA_MANAGEMENT_API_SECRET
+    ).stringValue;
+
+    const PRISMA_ENDPOINT = ssm.StringParameter.fromStringParameterName(
+      this,
+      'PRISMA_ENDPOINT',
+      environmentVarKeys.staging.PRISMA_ENDPOINT
+    ).stringValue;
+
     new ecs_patterns.ApplicationLoadBalancedEc2Service(
       this,
       props.stackName + 'GqlService',
       {
         cluster,
         memoryReservationMiB: 700,
-        cpu: 1024,
+        cpu: 2048,
         publicLoadBalancer: true,
         taskImageOptions: {
           image: ecs.ContainerImage.fromEcrRepository(props.ecrRepository),
           containerPort: 4000,
+          environment: {
+            PRISMA_SECRET,
+            PRISMA_ENDPOINT,
+            PRISMA_MANAGEMENT_API_SECRET,
+            APP_SECRET,
+          },
         },
       }
     );
