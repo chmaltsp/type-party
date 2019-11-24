bdcbad098a1f602bfd5da0e07c7e87d0ee584fd9
chmaltsp <chmaltsp@gmail.com>
2019-11-24 02:40:24 -0500

    migration staging

--- infrastructure/lib/tp-gql.ts
+++ infrastructure/lib/tp-gql.ts
@@ -31,29 +31,37 @@ export class TpGql extends cdk.Stack {
       },
     };
 
-    const PRISMA_SECRET = ssm.StringParameter.fromStringParameterName(
-      this,
-      'PRISMA_SECRET',
-      environmentVarKeys.staging.PRISMA_SECRET
-    ).stringValue;
+    const PRISMA_SECRET = ecs.Secret.fromSsmParameter(
+      ssm.StringParameter.fromStringParameterName(
+        this,
+        'PRISMA_SECRET',
+        environmentVarKeys.staging.PRISMA_SECRET
+      )
+    );
 
-    const APP_SECRET = ssm.StringParameter.fromStringParameterName(
-      this,
-      'APP_SECRET',
-      environmentVarKeys.staging.APP_SECRET
-    ).stringValue;
+    const APP_SECRET = ecs.Secret.fromSsmParameter(
+      ssm.StringParameter.fromStringParameterName(
+        this,
+        'APP_SECRET',
+        environmentVarKeys.staging.APP_SECRET
+      )
+    );
 
-    const PRISMA_MANAGEMENT_API_SECRET = ssm.StringParameter.fromStringParameterName(
-      this,
-      'PRISMA_MANAGEMENT_API_SECRET',
-      environmentVarKeys.staging.PRISMA_MANAGEMENT_API_SECRET
-    ).stringValue;
+    const PRISMA_MANAGEMENT_API_SECRET = ecs.Secret.fromSsmParameter(
+      ssm.StringParameter.fromStringParameterName(
+        this,
+        'PRISMA_MANAGEMENT_API_SECRET',
+        environmentVarKeys.staging.PRISMA_MANAGEMENT_API_SECRET
+      )
+    );
 
-    const PRISMA_ENDPOINT = ssm.StringParameter.fromStringParameterName(
-      this,
-      'PRISMA_ENDPOINT',
-      environmentVarKeys.staging.PRISMA_ENDPOINT
-    ).stringValue;
+    const PRISMA_ENDPOINT = ecs.Secret.fromSsmParameter(
+      ssm.StringParameter.fromStringParameterName(
+        this,
+        'PRISMA_ENDPOINT',
+        environmentVarKeys.staging.PRISMA_ENDPOINT
+      )
+    );
 
     new ecs_patterns.ApplicationLoadBalancedEc2Service(
       this,
@@ -61,12 +69,11 @@ export class TpGql extends cdk.Stack {
       {
         cluster,
         memoryReservationMiB: 700,
-        cpu: 2048,
         publicLoadBalancer: true,
         taskImageOptions: {
           image: ecs.ContainerImage.fromEcrRepository(props.ecrRepository),
           containerPort: 4000,
-          environment: {
+          secrets: {
             PRISMA_SECRET,
             PRISMA_ENDPOINT,
             PRISMA_MANAGEMENT_API_SECRET,
