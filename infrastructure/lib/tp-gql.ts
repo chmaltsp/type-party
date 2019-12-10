c6bc6c03b7da0aba2b29054a9024a64bef89b363
chmaltsp <chmaltsp@gmail.com>
2019-12-09 22:48:25 -0500

    remove bucket from param

--- infrastructure/lib/tp-gql.ts
+++ infrastructure/lib/tp-gql.ts
@@ -76,7 +76,7 @@ export class ApiStack extends cdk.Stack {
 
     const S3_BUCKET = props.imageBucket.bucketName;
 
-    const S3_ENDPOINT = props.imageBucket.bucketDomainName;
+    const S3_ENDPOINT = `https://${props.imageBucket.bucketRegionalDomainName}`;
     const apiService = new ecs_patterns.ApplicationLoadBalancedEc2Service(
       this,
       'service',
