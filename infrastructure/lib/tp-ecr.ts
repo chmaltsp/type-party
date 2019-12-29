2103460fa6c08854801c5c9f30bc43b2554a6fd5
chmaltsp <chmaltsp@gmail.com>
2019-12-29 15:27:00 -0600

    redeploy

--- infrastructure/lib/tp-ecr.ts
+++ infrastructure/lib/tp-ecr.ts
@@ -2,16 +2,16 @@ import ecr = require('@aws-cdk/aws-ecr');
 
 import cdk = require('@aws-cdk/core');
 
-export const TP_GQL_REPO = 'tp-gql';
+export const TP_API_REPO = 'tp-api';
 export const TP_FE_REPO = 'tp-fe';
 export class TpEcr extends cdk.Stack {
-  public readonly gql: ecr.IRepository;
+  public readonly api: ecr.IRepository;
   public readonly fe: ecr.IRepository;
   constructor(parent: cdk.App, name: string) {
     super(parent, name);
 
-    this.gql = new ecr.Repository(this, TP_GQL_REPO, {
-      repositoryName: TP_GQL_REPO,
+    this.api = new ecr.Repository(this, TP_API_REPO, {
+      repositoryName: TP_API_REPO,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       lifecycleRules: [
         {
