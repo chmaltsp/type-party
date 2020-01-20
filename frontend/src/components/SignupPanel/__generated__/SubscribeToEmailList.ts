8f2297f1002a4808799bfc92d425893c32d6aa16
chmaltsp <chmaltsp@gmail.com>
2020-01-20 12:22:18 -0500

    add basic FE

---
+++ frontend/src/components/SignupPanel/__generated__/SubscribeToEmailList.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: SubscribeToEmailList
+// ====================================================
+
+export interface SubscribeToEmailList_subscribeToEmailList {
+  __typename: "MailchimpPayload";
+  status: string;
+}
+
+export interface SubscribeToEmailList {
+  subscribeToEmailList: SubscribeToEmailList_subscribeToEmailList;
+}
+
+export interface SubscribeToEmailListVariables {
+  email: string;
+}
