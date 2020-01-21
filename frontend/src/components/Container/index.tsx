be9adf8bd43a6a419027398913d5cf4fd53cad05
chmaltsp <chmaltsp@gmail.com>
2020-01-21 03:07:10 -0500

    add margin top

--- frontend/src/components/Container/index.tsx
+++ frontend/src/components/Container/index.tsx
@@ -3,6 +3,7 @@ import styled, { media } from 'sc';
 
 export const Container = styled.div`
   padding: 0 ${em(136)};
+  margin-top: ${em(96)};
   ${media.desktop`
         padding: 0 ${em(32)};
     `};
