deca5e71b6e3f246f3b491923afaed8e19a2e133
chmaltsp <chmaltsp@gmail.com>
2020-06-30 05:13:19 -0400

    add pure carousel slider

--- frontend/src/components/Container/index.tsx
+++ frontend/src/components/Container/index.tsx
@@ -8,3 +8,10 @@ export const Container = styled.div`
         padding: 0 ${em(32)};
     `};
 `;
+
+export const FullWidthContainer = styled(Container)`
+  padding: 0;
+  ${media.desktop`
+        padding: 0;
+    `};
+`;
--- frontend/src/pages/Home/HeroCarousel/index.tsx
+++ frontend/src/pages/Home/HeroCarousel/index.tsx
@@ -1,16 +1,64 @@
 import * as React from 'react';
 import { ChildDataProps, graphql } from 'react-apollo';
-import {
-  GetFeaturedWebsites,
-  GetFeaturedWebsites_websites,
-} from '../__generated__/GetFeaturedWebsites';
+import { GetFeaturedWebsites } from '../__generated__/GetFeaturedWebsites';
 import { GET_FEATURED_WEBSITES } from '../queries';
+import { selectCards } from '../Websites';
+
+import leftArrow from './LeftArrow.svg';
+import rightArrow from './RightArrow.svg';
+
+import { CarouselProvider, Slide, Slider } from 'pure-react-carousel';
+
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import {
+  ButtonBack,
+  ButtonNext,
+  Card,
+  DotGroup,
+  LeftArrow,
+  RightArrow,
+  Wrapper,
+} from './styles';
 
 const HeroCarousel: React.FunctionComponent<
-  ChildDataProps<GetFeaturedWebsites>
+  ChildDataProps<{}, GetFeaturedWebsites>
 > = props => {
-  console.log(props);
-  return <div>Carousel</div>;
+  const websites = props.data && props.data.websites;
+
+  if (!websites) {
+    return null;
+  }
+
+  const cards = selectCards(websites);
+  return (
+    <Wrapper>
+      <CarouselProvider
+        naturalSlideWidth={100}
+        naturalSlideHeight={100}
+        visibleSlides={1}
+        totalSlides={cards.length}
+        infinite={true}
+        isIntrinsicHeight={true}
+        interval={5000}
+        // isPlaying={true}
+      >
+        <Slider>
+          {cards.map((card, idx) => (
+            <Slide key={idx} index={idx}>
+              <Card key={card.id} {...card} />
+            </Slide>
+          ))}
+        </Slider>
+        <ButtonBack>
+          <LeftArrow src={leftArrow} />
+        </ButtonBack>
+        <ButtonNext>
+          <RightArrow src={rightArrow} />
+        </ButtonNext>
+        <DotGroup />
+      </CarouselProvider>
+    </Wrapper>
+  );
 };
 
 const WrappedCarousel = graphql<{}, GetFeaturedWebsites, any, any>(GET_FEATURED_WEBSITES)(
